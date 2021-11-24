88bef5c39efb7166755e648eb4c995530e03687a
pwalsh2 <pwalsh2@unb.ca>
2021-11-24 18:16:23 -0400

    Changed url

--- src/makeData.js
+++ src/makeData.js
@@ -1,5 +1,9 @@
 export  async function makeData(setData) {
-    const url = 'http://127.0.0.1:8000/api/student_data';
+
+  let getURL = window.location.hostname;
+  console.log(getURL)
+  if(getURL === 'localhost' || getURL === '127.0.0.1'){
+    const url = 'http://' + getURL + ':8000/api/student_data';
     const response = await fetch(url);
     const data = await response.json();
     const final_data = await data.map(row => {
@@ -10,15 +14,41 @@ export  async function makeData(setData) {
     return setData(final_data)
   }
 
-  export async function makeTranscript(key, setData) {
-    if(key.length !== 0){
-      console.log(key);
-      const url = "http://127.0.0.1:8000/get_transcript/" + key[0] + "";
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
+    const url = 'http://' + getURL +'/api/student_data';
+    const response = await fetch(url);
+    const data = await response.json();
+    const final_data = await data.map(row => {
+      
+      return row.fields
+  })
+  console.log(final_data)
+    return setData(final_data)
   
-      return setData(data);
+  }
+
+  export async function makeTranscript(key, setData) {
+
+    let getURL = window.location.hostname;
+    if(key.length !== 0){
+      if(getURL === 'localhost' || getURL === '127.0.0.1'){
+      
+        const url = "http://"+ getURL + ":8000/get_transcript/" + key[0] + "";
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    
+        return setData(data);
+      }
+      else{
+        console.log(key);
+        const url = "http://"+ getURL + "/get_transcript/" + key[0] + "";
+        const response = await fetch(url);
+        const data = await response.json();
+      
+    
+        return setData(data);
+      }
+    
     }
 
 	}
